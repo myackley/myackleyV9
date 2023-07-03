606369dc669994cbb2f3c11b1788d4c3361c6d23
Mark Yackley <myackley@hotmail.com>
2023-07-03 15:27:32 -0500

    Updating footer to use block pattern for easy updating, created 3 column footer

---
+++ patterns/footer-default.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: Footer, default
+ * Slug: myackleyV9/footer-default
+ * Categories: footer
+ * Block Types: core/post-content
+ */
+?>
+<!-- wp:group {"style":{"backgroundColor":"#222222","layout":{"type":"constrained"}},"className":"has-black-background-color"} -->
+<div class="wp-block-group has-black-background-color"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40"}}},"backgroundColor":"black","className":"has-dark-background-color","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"nowrap"},"fontSize":"small"} -->
+<div class="wp-block-group alignwide has-dark-background-color has-black-background-color has-background has-small-font-size" style="padding-top:var(--wp--preset--spacing--40)"><!-- wp:paragraph {"textColor":"white"} -->
+<p class="has-white-color has-text-color">© Copyright 2023</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|30","left":"var:preset|spacing|30"}}}} -->
+<ul class="wp-block-social-links"><!-- wp:social-link {"url":"https://www.linkedin.com/in/markyackley","service":"linkedin"} /-->
+
+<!-- wp:social-link {"url":"https://github.com/myackley","service":"github"} /-->
+
+<!-- wp:social-link {"url":"https://codepen.io/myackley35/","service":"codepen"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ patterns/footer-three-cols.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Title: Footer, three columns
+ * Slug: myackleyV9/footer-three-cols
+ * Categories: footer
+ * Block Types: core/post-content
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"1rem","left":"1rem","right":"var:preset|spacing|40"}}},"backgroundColor":"#222","className":"has-black-background-color","layout":{"type":"constrained","wideSize":"100%"}} -->
+<div class="wp-block-group has-black-background-color has-222-background-color has-background" style="padding-top:1rem;padding-right:var(--wp--preset--spacing--40);padding-left:1rem">
+<!-- wp:spacer {"height":"14px"} -->
+<div style="height:14px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+<!-- wp:columns {"verticalAlignment":"center"} -->
+<div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"33%","textColor":"white","layout":{"type":"default"}} -->
+<div class="wp-block-column is-vertically-aligned-center has-white-color has-text-color" style="flex-basis:33%"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1.5"}},"textColor":"white"} -->
+<p class="has-text-align-center has-white-color has-text-color" style="line-height:1.5">© Copyright 2023A</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"33%","textColor":"white"} -->
+<div class="wp-block-column is-vertically-aligned-center has-white-color has-text-color" style="flex-basis:33%"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"33%","textColor":"white"} -->
+<div class="wp-block-column is-vertically-aligned-center has-white-color has-text-color" style="flex-basis:33%"><!-- wp:social-links {"openInNewTab":true,"layout":{"type":"flex","justifyContent":"center"}} -->
+<ul class="wp-block-social-links"><!-- wp:social-link {"url":"https://www.linkedin.com/in/markyackley","service":"linkedin"} /-->
+
+<!-- wp:social-link {"url":"https://github.com/myackley","service":"github"} /-->
+
+<!-- wp:social-link {"url":"https://codepen.io/myackley35/","service":"codepen"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
